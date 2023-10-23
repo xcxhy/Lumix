1c6855aaf82cbf4c1aa7572dd0d7b7ff6dfb87fa
xcxhy <1641173099@qq.com>
2023-10-23 20:48:24 +0800

    commit

--- Filter/filter_utils.py
+++ Filter/filter_utils.py
@@ -7,4 +7,17 @@ from multiprocessing import Pool
 from concurrent import futures
 
 
+# filter messy code
+def filter_messy_code_text(data):
+    '''
+    data: a list of text
+    '''
+    start = time.time()
+    print('Start filtering messy code text...')
+    positive_list, negative_list = fasttext_format_inference_singel(data)
+    end = time.time()
+    print('Filtering messy code text done, time cost: %.2f s' % (end - start))
+    
+    return positive_list
+
 
