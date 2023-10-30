5385cdb2634017d60cb568aaa543a7140da256d5
xcxhy <1641173099@qq.com>
2023-10-30 13:28:04 +0800

    commit

--- Classification/train.py
+++ Classification/train.py
@@ -7,12 +7,12 @@ import fasttext
 from utils import *
 
 
-def train(model_path, save_path):
-    model = fasttext.train_supervised(input="/home/xuhao/xcxhy/Focus_Classification/dataset/meaning/quality_train.txt", wordNgrams=5)
+def train(text_path, model_path):
+    model = fasttext.train_supervised(input=text_path, wordNgrams=5)
 
-    model.save_model("/home/xuhao/xcxhy/Focus_Classification/model/judge_content_quality_120k_v3.bin")
+    model.save_model(model_path)
 
 if __name__=="__main__":
-    model_path = "/home/xuhao/xcxhy/Focus_Classification/model/judge_content_quality_120k_v3.bin"
-    save_path = "/home/xuhao/xcxhy/Focus_Classification/model"
-    train(model_path, save_path)
+    text_path = "Data/meaning/quality_train.txt"
+    model_path = "/home/xuhao/xcxhy/Focus_Classification/model"
+    train(text_path, model_path)
